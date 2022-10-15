16882458f1666d64cbb6c5dec5d430adf24e479f
ankit-pn <miankitsingh@gmail.com>
2022-10-15 09:49:15 +0530

    added test_6

---
+++ test_6/target/debug/deps/test_6-8bd313798f34f677.d
@@ -0,0 +1,5 @@
+/home/pain/Documents/Rust-Learning/test_6/target/debug/deps/test_6-8bd313798f34f677: src/main.rs
+
+/home/pain/Documents/Rust-Learning/test_6/target/debug/deps/test_6-8bd313798f34f677.d: src/main.rs
+
+src/main.rs:
---
+++ test_6/target/debug/test_6.d
@@ -0,0 +1,1 @@
+/home/pain/Documents/Rust-Learning/test_6/target/debug/test_6: /home/pain/Documents/Rust-Learning/test_6/src/main.rs
